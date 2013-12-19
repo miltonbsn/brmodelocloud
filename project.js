229abf9aea9f4f74d0c5b4554d70c8e7c0763518
miltonbsn <miltonbst@gmail.com>
2013-12-19 19:26:09 -0200

    Validações em login  inicio tela de registro

--- project.js
+++ project.js
@@ -11,6 +11,9 @@ app.config(['$routeProvider', '$locationProvider',
         when('/workspace', {
             templateUrl: 'workspace.html'
         }).
+        when('/register', {
+            templateUrl: 'register.html'
+        }).
         otherwise({
             redirectTo: '/'
         });
@@ -20,9 +23,39 @@ app.config(['$routeProvider', '$locationProvider',
 app.controller('loginController', function($scope, $location){
         
     $scope.login = function(user){
-      
-      console.log(user);
-      $location.path('/workspace').replace();
-
-    };
+    
+    if(validLoginFields(user)==true){
+        console.log(user);
+        $location.path('/workspace').replace();
+    }
+    
+    }
 });
+
+app.$inject = ['$scope', '$http'];
+
+function validLoginFields(){
+    var valid = true;
+    var email = $("#emailfield");
+    var password = $("#passwordfield");
+    
+        if(email.val() == ""){
+            $("#emailfield").addClass("error");
+            valid = false;
+        } else {
+            $("#emailfield").removeClass("error");
+        }
+            
+        if(password.val() == ""){
+            $("#passwordfield").addClass("error");
+            valid = false;
+        } else {
+            $("#passwordfield").removeClass("error");
+        }
+    
+    return valid;
+}
+
+
+
+
