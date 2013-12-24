57dafeaad328a68afa5eabd4484e67d2d451939b
miltonbsn <miltonbst@gmail.com>
2013-12-24 00:43:56 -0200

    Telade login e registro feias + evolução no angularjs

--- project.js
+++ project.js
@@ -1,61 +1,73 @@
-var app = angular.module('myapp', ['ngRoute']);
+  var app = angular.module('myapp', ['ngRoute']);
 
-app.config(['$routeProvider', '$locationProvider',
-    function($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode(true);
-        $routeProvider.
-        when('/', {
-            controller:'loginController',
-            templateUrl: 'login.html'
-        }).
-        when('/workspace', {
-            templateUrl: 'workspace.html'
-        }).
-        when('/register', {
-            templateUrl: 'register.html'
-        }).
-        otherwise({
-            redirectTo: '/'
-        });
+  app.config(['$routeProvider', '$locationProvider',
+      function ($routeProvider, $locationProvider) {
+      $locationProvider.html5Mode(true);
+      $routeProvider.
+      when('/', {
+        controller: 'loginController',
+        templateUrl: 'login.html'
+      }).
+      when('/workspace', {
+        controller: 'workspaceController',
+        templateUrl: 'workspace.html'
+      }).
+      when('/register', {
+        controller: 'registerController',
+        templateUrl: 'register.html'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+      }
+  ]);
+
+
+  app.controller('loginController', function ($scope, $location) {
+
+    $scope.register = function () {
+      $location.path('/register').replace();
     }
-]);
 
-app.controller('loginController', function($scope, $location){
-        
-    $scope.login = function(user){
-    
-    if(validLoginFields(user)==true){
+    $scope.login = function (user) {
+
+      if (validLoginFields(user) == true) {
         console.log(user);
         $location.path('/workspace').replace();
-    }
-    
-    }
-});
+      }
 
-app.$inject = ['$scope', '$http'];
+    }
+  });
 
-function validLoginFields(){
+  app.controller('registerController', function ($scope, $location) {
+
+    $scope.login = function () {
+      $location.path('/').replace();
+    }
+
+
+  });
+
+  app.$inject = ['$scope', '$http'];
+
+  function validLoginFields() {
     var valid = true;
     var email = $("#emailfield");
     var password = $("#passwordfield");
-    
-        if(email.val() == ""){
-            $("#emailfield").addClass("error");
-            valid = false;
-        } else {
-            $("#emailfield").removeClass("error");
-        }
-            
-        if(password.val() == ""){
-            $("#passwordfield").addClass("error");
-            valid = false;
-        } else {
-            $("#passwordfield").removeClass("error");
-        }
-    
+
+    if (email.val() == "") {
+      $("#emailfield").addClass("error");
+      valid = false;
+    } else {
+      $("#emailfield").removeClass("error");
+    }
+
+    if (password.val() == "") {
+      $("#passwordfield").addClass("error");
+      valid = false;
+    } else {
+      $("#passwordfield").removeClass("error");
+    }
+
     return valid;
-}
-
-
-
-
+  }
